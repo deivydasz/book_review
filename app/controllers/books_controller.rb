80aa7128cd21b9502fddb1f846909376729b0ef5
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-09-21 13:51:50 +0300

    Added raty stars to review system

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
 	before_action :find_book, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit] 
 
 	def index
 		if params[:category].blank?
@@ -10,7 +11,12 @@ class BooksController < ApplicationController
 		end
 	end
 
-	def show		
+	def show	
+		if @book.reviews.blank?
+			@average_review = 0
+		else
+			@average_review = @book.reviews.average(:rating).round(2)
+		end
 	end
 
 	def new
